92e00f4ac70105a6aa4909d4af039e406dea1aef
Anthony <38259824+arn-foto@users.noreply.github.com>
2020-06-08 10:07:24 -0700

    Update CheckoutForm.test.js

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
+//As with any library, we need to import react-testing-library before we can use it,
 import CheckoutForm from "./CheckoutForm";
 import ReactDOM from "react-dom";
 import App from "../App";
 
 // Write up the two tests here and make sure they are testing what the title shows
+
+//Automated testing minimizes the risk of bugs finding their way into production code. Testing should be apart of every workflow
+// this particular test works on 3 methonds....arrange, act, & assert. run with " npm test -" in the console to execute test
 
 test("form header renders", () => {});
 const { getByText } = render(<CheckoutForm />);
@@ -12,10 +16,7 @@ const header = getByText(/checkout form/i);
 expect(header).toBeInTheDocument();
 
 test("form shows success message on submit with form details", () => {
-  // const {getLabelByText} = render(<CheckoutForm/>);
-  // fireEvent.change(getLabelByText(/first name/i), {
-  // target: {value: "Andrew"}
-  // });
+
 });
 
 test("renders without crashing", () => {
