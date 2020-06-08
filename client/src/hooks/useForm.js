f9623af19a0a8accec9358d00b1aa11e5ce01e49
Anthony <38259824+arn-foto@users.noreply.github.com>
2020-06-08 10:08:35 -0700

    Update useForm.js

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,6 +1,7 @@
 // write your custom hook here to control your checkout form
 import { useState } from "react";
 
+// form object
 const initialValue = {
   firstName: "",
   lastName: "",
@@ -10,9 +11,12 @@ const initialValue = {
   zip: "",
 };
 
+//this custom hook  to apply non-visual behavior and stateful logic throughout my components.
+//This way I can reuse the same hook over and over again
 export const useForm = () => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [values, setValues] = useState(initialValue);
+
   // this handle takes the current name value as an event and updates its current state
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
